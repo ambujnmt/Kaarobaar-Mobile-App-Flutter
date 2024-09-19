d6cc82ed15425f028080f0fbd36b97ec2cb03e21
ambujnmt <ambuj@nmttechnologies.com>
2024-09-19 18:29:01 +0530

    Add Business UI form changes and api integration

--- lib/views/screens/add_business.dart
+++ lib/views/screens/add_business.dart
@@ -22,6 +22,7 @@ class _AddBusinessState extends State<AddBusiness> {
   TextEditingController emailController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
   TextEditingController postalCodeController = TextEditingController();
+  TextEditingController websiteURL = TextEditingController();
   TextEditingController businessDescriptionController = TextEditingController();
   TextEditingController businessTypeController = TextEditingController();
   TextEditingController businessAddressController = TextEditingController();
@@ -31,35 +32,52 @@ class _AddBusinessState extends State<AddBusiness> {
   addBusiness() async {
     if (businessNameController.text.isNotEmpty &&
         (!businessNameController.text.startsWith(" "))) {
-      if (_dropdownSearchFieldController.text.isNotEmpty &&
-          !_dropdownSearchFieldController.text.startsWith(" ")) {
+      if (categoryDropdownController.text.isNotEmpty &&
+          !categoryDropdownController.text.startsWith(" ")) {
         if (businessKeywordController.text.isNotEmpty &&
             !businessKeywordController.text.startsWith(" ")) {
           if (EmailValidator.validate(emailController.text)) {
             if (phoneController.text.length == 10) {
-              if (postalCodeController.text.isEmpty &&
+              if (postalCodeController.text.isNotEmpty &&
                   !postalCodeController.text.startsWith(" ")) {
-                if (businessDescriptionController.text.isNotEmpty &&
-                    (!businessDescriptionController.text.startsWith(" "))) {
-                  if (businessTypeController.text.isNotEmpty &&
-                      (!businessTypeController.text.startsWith(" "))) {
-                    if (businessAddressController.text.isNotEmpty &&
-                        (!businessAddressController.text.startsWith(" "))) {
-                      if (vatNoController.text.isNotEmpty &&
-                          (!vatNoController.text.startsWith(" "))) {
+                if (websiteURL.text.isNotEmpty &&
+                    !websiteURL.text.startsWith(" ")) {
+                  if (businessDescriptionController.text.isNotEmpty &&
+                      (!businessDescriptionController.text.startsWith(" "))) {
+                    if (businessTypeController.text.isNotEmpty &&
+                        (!businessTypeController.text.startsWith(" "))) {
+                      if (stateDropdownController.text.isNotEmpty &&
+                          !stateDropdownController.text.startsWith(" ")) {
+                        if (cityDropdownController.text.isNotEmpty &&
+                            !cityDropdownController.text.startsWith(" ")) {
+                          if (businessAddressController.text.isNotEmpty &&
+                              (!businessAddressController.text
+                                  .startsWith(" "))) {
+                            if (vatNoController.text.isNotEmpty &&
+                                (!vatNoController.text.startsWith(" "))) {
+                            } else {
+                              helper.errorDialog(
+                                  context, "Please enter vat number");
+                            }
+                          } else {
+                            helper.errorDialog(
+                                context, "Please enter business address");
+                          }
+                        } else {
+                          helper.errorDialog(context, 'Please select city');
+                        }
                       } else {
-                        helper.errorDialog(context, "Please enter vat number");
+                        helper.errorDialog(context, 'Please select state');
                       }
                     } else {
-                      helper.errorDialog(
-                          context, "Please enter business address");
+                      helper.errorDialog(context, "Please enter business type");
                     }
                   } else {
-                    helper.errorDialog(context, "Please enter business type");
+                    helper.errorDialog(
+                        context, "Please enter business description");
                   }
                 } else {
-                  helper.errorDialog(
-                      context, "Please enter business description");
+                  helper.errorDialog(context, "Please enter website URL");
                 }
               } else {
                 helper.errorDialog(context, "Please enter postal code");
@@ -98,25 +116,68 @@ class _AddBusinessState extends State<AddBusiness> {
     // }
   }
 
-  String? _selectedFruit;
+  String? selectedCategory;
+  String? selectedState;
+  String? selectedCity;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _dropdownSearchFieldController =
+  final TextEditingController categoryDropdownController =
       TextEditingController();
-  SuggestionsBoxController suggestionBoxController = SuggestionsBoxController();
+  final TextEditingController stateDropdownController = TextEditingController();
+  final TextEditingController cityDropdownController = TextEditingController();
 
-  static List<String> getSuggestions(String query) {
-    List<String> matches = <String>[];
-    final List<String> fruits = [
-      'Apple',
-      'Avocado',
-      'Banana',
-      'Blueberries',
-      'Blackberries',
+  SuggestionsBoxController categorySuggestionBoxController =
+      SuggestionsBoxController();
+  SuggestionsBoxController stateSuggestionBoxController =
+      SuggestionsBoxController();
+  SuggestionsBoxController citySuggestionBoxController =
+      SuggestionsBoxController();
+
+  static List<String> getCategorySuggestions(String query) {
+    List<String> categoryMatches = <String>[];
+    final List<String> categoryList = [
+      'IT',
+      'Banking',
+      'Medical',
+      'Education',
+      'Institute',
     ];
-    matches.addAll(fruits);
+    categoryMatches.addAll(categoryList);
 
-    matches.retainWhere((s) => s.toLowerCase().contains(query.toLowerCase()));
-    return matches;
+    categoryMatches
+        .retainWhere((s) => s.toLowerCase().contains(query.toLowerCase()));
+    return categoryMatches;
+  }
+
+  static List<String> getStateSuggestions(String query) {
+    List<String> stateMatches = <String>[];
+    final List<String> stateList = [
+      'Uttar Pradesh',
+      'Noida',
+      'Andra Pradesh',
+      'Delhi',
+      'Lucknow',
+    ];
+    stateMatches.addAll(stateList);
+
+    stateMatches
+        .retainWhere((s) => s.toLowerCase().contains(query.toLowerCase()));
+    return stateMatches;
+  }
+
+  static List<String> getCitySuggestions(String query) {
+    List<String> cityMatches = <String>[];
+    final List<String> cityList = [
+      'City1',
+      'City2',
+      'City3',
+      'City4',
+      'City5',
+    ];
+    cityMatches.addAll(cityList);
+
+    cityMatches
+        .retainWhere((s) => s.toLowerCase().contains(query.toLowerCase()));
+    return cityMatches;
   }
 
   @override
@@ -151,7 +212,7 @@ class _AddBusinessState extends State<AddBusiness> {
                   SizedBox(
                     height: size.width * 0.05,
                   ),
-                  // place your dropdown here
+                  // category dropdown
                   DropDownSearchFormField(
                     textFieldConfiguration: TextFieldConfiguration(
                       decoration: InputDecoration(
@@ -167,10 +228,10 @@ class _AddBusinessState extends State<AddBusiness> {
                         hintStyle: customText.kTextStyle(
                             16, FontWeight.w400, ColorConstants.kIconsGrey),
                       ),
-                      controller: _dropdownSearchFieldController,
+                      controller: categoryDropdownController,
                     ),
                     suggestionsCallback: (pattern) {
-                      return getSuggestions(pattern);
+                      return getCategorySuggestions(pattern);
                     },
                     itemBuilder: (context, String suggestion) {
                       return ListTile(
@@ -184,12 +245,12 @@ class _AddBusinessState extends State<AddBusiness> {
                       return suggestionsBox;
                     },
                     onSuggestionSelected: (String suggestion) {
-                      _dropdownSearchFieldController.text = suggestion;
+                      categoryDropdownController.text = suggestion;
                     },
-                    suggestionsBoxController: suggestionBoxController,
+                    suggestionsBoxController: categorySuggestionBoxController,
                     validator: (value) =>
-                        value!.isEmpty ? 'Please select a fruit' : null,
-                    onSaved: (value) => _selectedFruit = value,
+                        value!.isEmpty ? 'Please select a category' : null,
+                    onSaved: (value) => selectedCategory = value,
                     displayAllSuggestionWhenTap: true,
                   ),
                   SizedBox(
@@ -238,11 +299,25 @@ class _AddBusinessState extends State<AddBusiness> {
                     height: size.width * 0.05,
                   ),
                   TextField(
-                    keyboardType: TextInputType.number,
                     textInputAction: TextInputAction.next,
                     controller: postalCodeController,
                     decoration: InputDecoration(
                       hintText: "Postal Code",
+                      hintStyle: customText.kTextStyle(
+                          16, FontWeight.w400, ColorConstants.kIconsGrey),
+                      // prefixIcon: const Icon(Icons.phone_android, color: ColorConstants.kIconsGrey, size: 35,),
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: size.width * 0.05,
+                  ),
+
+                  TextField(
+                    textInputAction: TextInputAction.next,
+                    controller: websiteURL,
+                    decoration: InputDecoration(
+                      hintText: "Website URL",
                       hintStyle: customText.kTextStyle(
                           16, FontWeight.w400, ColorConstants.kIconsGrey),
                       // prefixIcon: const Icon(Icons.phone_android, color: ColorConstants.kIconsGrey, size: 35,),
@@ -279,6 +354,94 @@ class _AddBusinessState extends State<AddBusiness> {
                           16, FontWeight.w400, ColorConstants.kIconsGrey),
                       // prefixIcon: const Icon(Icons.category, color: ColorConstants.kIconsGrey, size: 35,),
                     ),
+                  ),
+                  SizedBox(
+                    height: size.width * 0.05,
+                  ),
+                  // state dropdown
+                  DropDownSearchFormField(
+                    textFieldConfiguration: TextFieldConfiguration(
+                      decoration: InputDecoration(
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromRGBO(112, 112, 112, 1),
+                          ),
+                        ),
+                        border: const UnderlineInputBorder(
+                          borderSide: BorderSide(),
+                        ),
+                        hintText: 'Select State',
+                        hintStyle: customText.kTextStyle(
+                            16, FontWeight.w400, ColorConstants.kIconsGrey),
+                      ),
+                      controller: stateDropdownController,
+                    ),
+                    suggestionsCallback: (pattern) {
+                      return getStateSuggestions(pattern);
+                    },
+                    itemBuilder: (context, String suggestion) {
+                      return ListTile(
+                        title: Text(suggestion),
+                      );
+                    },
+                    itemSeparatorBuilder: (context, index) {
+                      return const Divider();
+                    },
+                    transitionBuilder: (context, suggestionsBox, controller) {
+                      return suggestionsBox;
+                    },
+                    onSuggestionSelected: (String suggestion) {
+                      stateDropdownController.text = suggestion;
+                    },
+                    suggestionsBoxController: stateSuggestionBoxController,
+                    validator: (value) =>
+                        value!.isEmpty ? 'Please select a state' : null,
+                    onSaved: (value) => selectedState = value,
+                    displayAllSuggestionWhenTap: true,
+                  ),
+                  SizedBox(
+                    height: size.width * 0.05,
+                  ),
+                  // city dropdown
+                  DropDownSearchFormField(
+                    textFieldConfiguration: TextFieldConfiguration(
+                      decoration: InputDecoration(
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromRGBO(112, 112, 112, 1),
+                          ),
+                        ),
+                        border: const UnderlineInputBorder(
+                          borderSide: BorderSide(),
+                        ),
+                        hintText: 'Select City',
+                        hintStyle: customText.kTextStyle(
+                            16, FontWeight.w400, ColorConstants.kIconsGrey),
+                      ),
+                      controller: cityDropdownController,
+                    ),
+                    suggestionsCallback: (pattern) {
+                      return getCitySuggestions(pattern);
+                    },
+                    itemBuilder: (context, String suggestion) {
+                      return ListTile(
+                        title: Text(suggestion),
+                      );
+                    },
+                    itemSeparatorBuilder: (context, index) {
+                      return const Divider();
+                    },
+                    transitionBuilder: (context, suggestionsBox, controller) {
+                      return suggestionsBox;
+                    },
+                    onSuggestionSelected: (String suggestion) {
+                      cityDropdownController.text = suggestion;
+                    },
+                    suggestionsBoxController: citySuggestionBoxController,
+                    validator: (value) =>
+                        value!.isEmpty ? 'Please select a city' : null,
+                    onSaved: (value) => selectedCity = value,
+                    displayAllSuggestionWhenTap: true,
                   ),
                   SizedBox(
                     height: size.width * 0.05,
